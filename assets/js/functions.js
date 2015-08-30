3c90ced406b52dd1d0e5f3307da58cbbd903c1df
Sam Deacon <deaconsam@hotmail.co.uk>
2015-08-31 00:48:57 +0100

    started styling the blog section

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,17 +6,24 @@ function mentoringBubbleClick(){
   $('.face').on('click',function(){
     var $this = $(this),
         faceTop = $(this).position().top,
-        vertMath = -1*(faceTop - 220);
+        vertMath = -1*(faceTop - 230),
+        faceLeft = $this.position().left,
+        horzMath = 0 - faceLeft;
 
-    $this.parent().css('top', + vertMath + 'px');
-
+    if ($(window).width() > 640){
+      $this.parent().css('top', + vertMath + 'px');
+    } else {
+      if($this.hasClass('back-btn')){
+        mentoringNarrowStart();
+      }else{
+        $this.parent().css('left', + horzMath + 'px');
+      }
+    }
+    if(!$this.hasClass('back-btn')){
     $this.addClass('has-bubble-open')
       .siblings().removeClass('has-bubble-open');
+    }
   });
-
-
-
-
 };
 
 
@@ -34,13 +41,46 @@ function youtubeVidScroll(){
 
 function startMentoring(){
   var wScroll = $(window).scrollTop();
-  if($('section.mentoring').offset().top - 455 < wScroll){
+  if($('section.mentoring').offset().top - $(window).height()/2 < wScroll){
+      if($(window).width() > 640){
       $('.faces').addClass('launched');
 
-      if($('.face').hasClass('has-bubble-open')){
-      setTimeout(function(){
-        $('.face:nth-child(3)').addClass('has-bubble-open');
-      }, 500);
+      if(!$('.face').hasClass('has-bubble-open') && !$this.hasClass('back-btn')){
+        setTimeout(function(){
+          $('.face:nth-child(3)').addClass('has-bubble-open');
+        }, 300);
+      }
+    } else {
+      mentoringNarrowStart();
     }
   }
 };
+
+function mentoringNarrowStart(){
+  $('.faces').css({
+    'top': '230px',
+    'left': '0px'
+  });
+    $('.face').first().addClass('has-bubble-open')
+      .siblings().removeClass('has-bubble-open');
+}
+
+function mentoringWideStart(){
+  $('.faces').css({
+    'top': '0px',
+    'left': '0px'
+  });
+    $('.face:nth-child(3)').first().addClass('has-bubble-open')
+      .siblings().removeClass('has-bubble-open');
+}
+
+
+
+
+$(window).resize(function(){
+  if ($(window).width() > 640){
+    mentoringWideStart();
+  }else {
+    mentoringNarrowStart();
+  }
+});
